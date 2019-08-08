85129c807619473f18d23bf0366fc81e575ddd11
Fernando Florenzano <f.florenzano94@gmail.com>
2019-08-08 16:11:29 -0400

    :star: base.py

---
+++ Actividades/AC00/base.py
@@ -0,0 +1,17 @@
+# Seccion para importar liberías
+
+class Cuadrado(object):
+	def __init__(self, arg):
+		# Completar método y borrar pass
+		pass
+	# Agregar métodos
+
+class Triangulo(object):
+	def __init__(self, arg):
+		# Completar método y borrar pass
+		pass		
+	# Agregar métodos
+		
+		
+if __name__ == '__main__':
+	# Crear instancias aquí
