03fdfda3e5fba9ae103fe32dcee166f3e7c519a0
Daniela Concha <daconcha@uc.cl>
2019-09-28 20:05:48 -0300

    [T02] Enuncido y archivos :four_leaf_clover:

---
+++ Tareas/T02/parametros_plantas.py
@@ -0,0 +1,13 @@
+import os
+
+# Información de los choclos
+TIEMPO_CHOCLOS = 180
+FASES_CHOCLOS = 7
+COSECHA_CHOCLOS = 3
+PATH_SPRITES_CHOCLOS = os.path.join("sprites", "cultivos", "choclo")
+
+# Información de las alcachofas
+TIEMPO_ALCACHOFAS = 60
+FASES_ALCACHOFAS = 6
+COSECHA_ALCACHOFAS = 1
+PATH_SPRITES_ALCACHOFAS = os.path.join("sprites", "cultivos", "alcachofas")
---
+++ Tareas/T02/parametros_precios.py
@@ -0,0 +1,16 @@
+# Precio de los recursos
+PRECIO_ALACACHOFAS = 10
+PRECIO_CHOCLOS = 10
+PRECIO_LEÑA = 20
+PRECIO_ORO = 40
+
+# Precio de las semillas
+PRECIO_SEMILLA_ALCACHOFAS = 1
+PRECIO_SEMILLA_CHOCLOS = 5
+
+# Precio de las herramientas
+PRECIO_HACHA = 50
+PRECIO_AZADA = 30
+
+# Otros
+PRECIO_TICKET = 1500
